8d7d028c951c2f9cbc1316bbdf9771954a606d01
Mikhail <lazarenko.mihail@gmail.com>
2022-12-02 21:35:28 +0300

    решена задача 12

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -1,12 +1,1 @@
-﻿Console.WriteLine("Введите число 1:");
-int numberA = Convert.ToInt32 (Console.ReadLine());
-Console.WriteLine("Введите число 2:");
-int numberB = Convert.ToInt32(Console.ReadLine());
-if (numberB * numberB == numberA)
-{
-    Console.WriteLine("Yes");
-}
-else
-{
-Console.WriteLine("No");
-}
+﻿
