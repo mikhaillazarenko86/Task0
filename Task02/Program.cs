82d44296080089886be58a5eb337753149214b80
Mikhail <lazarenko.mihail@gmail.com>
2022-12-02 19:55:18 +0300

    Task02

---
+++ Task02/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число 1:");
+int numberA = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Введите число 2:");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberB * numberB == numberA)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+Console.WriteLine("No");
+}
