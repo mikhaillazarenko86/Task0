b55deb932d5552b162288c041735e53d8c9dbc76
Mikhail <lazarenko.mihail@gmail.com>
2022-12-03 20:57:14 +0300

    finished homework task 13

---
+++ HWTask13/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+
+// int number = new Random().Next(1, 10000000);
+int number = new Random().Next(1, 1000);
+Console.WriteLine(number);
+ 
+    while (number >= 1000)
+    {
+     number = number / 10;    
+    }
+Console.WriteLine(number);
+
+if (number < 100)
+{
+  Console.WriteLine("Нет третьей цифры");  
+}
+else
+{
+int digit3 = number % 10;
+Console.WriteLine(digit3);
+}
+
+
