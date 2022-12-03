e51876ecf75ffa40887db4e439de8364b16a59a6
Mikhail <lazarenko.mihail@gmail.com>
2022-12-03 21:09:57 +0300

    finished homework task 15

---
+++ HWTask15/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите число от 1 до 7");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 7)
+{
+    Console.WriteLine("Введено неверное число");
+}
+else
+{
+    if (number > 5)
+    {
+        Console.WriteLine("Ура! Выходной!");
+    }
+    else
+    {
+       Console.WriteLine("Будний"); 
+    }
+}
