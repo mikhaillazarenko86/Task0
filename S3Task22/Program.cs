7dd7154daa4db0dfcff6495a49aeb46db3b3c326
Mikhail <lazarenko.mihail@gmail.com>
2022-12-08 20:08:00 +0300

    решено

---
+++ S3Task22/Program.cs
@@ -0,0 +1,10 @@
+﻿
+
+
+Console.WriteLine("введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+for (i = 1; i <= n; i++)
+{
+    Console.WriteLine($"квадрат числа от 1 до {n} {i * i}");
+}
