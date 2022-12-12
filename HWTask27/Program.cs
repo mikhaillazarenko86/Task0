f24ec058f144a9e03a2a245aff80f10afda4d3de
Mikhail <lazarenko.mihail@gmail.com>
2022-12-12 14:48:54 +0300

    finished homework task 27

---
+++ HWTask27/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+Console.WriteLine("введите число А");
+int a = Convert.ToInt32(Console.ReadLine());
+int d = a;
+string str = Convert.ToString(a);
+int lenght = str.Length;
+// Console.WriteLine(lenght);
+int[] array = new int[lenght+2];
+for (int i = 0; i < lenght + 2; i++)
+{
+// int c = a - b;
+int b = a % 10;
+if (b == 0)
+{
+    a = a / 10;
+}
+else
+{
+a = a - b;
+}
+array[i] = b;
+
+}
+int sum = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    sum += array[i];
+}
+Console.WriteLine($"Сумма чисел в числе {d} равна {sum}");
