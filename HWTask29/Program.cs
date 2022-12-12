b2ef30296ce25b3b6d9c47f70ef57584a3d75f3a
Mikhail <lazarenko.mihail@gmail.com>
2022-12-12 14:59:17 +0300

    finished homework task 29

---
+++ HWTask29/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. Данные вводятся с консоли пользователем
+int[] array = new int[8];
+for (int i = 0; i <= 7; i++)
+{
+Console.WriteLine($"Введите число {i + 1}");
+int b = Convert.ToInt32(Console.ReadLine());
+array[i] = b;
+}
+Console.WriteLine(string.Join(", ", array));
