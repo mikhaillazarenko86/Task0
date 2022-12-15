20db39183af2d89713b24576f9535b337481c6fd
Михаил Лазаренко <mihaillazarenko@MacBook-Air-Mihail.local>
2022-12-15 22:38:57 +0300

    finished homework task 38

---
+++ HWTask38/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 38: Задайте массив натуральных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+int[] Getarray(int length, int leftrange, int rightrange)
+{
+    int[] array = new int[length];
+    Random rand = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(leftrange, rightrange + 1);
+    }
+return array;
+}
+int length = 11;
+int leftrange = 1;
+int rightrange = 20;
+int[] massive = Getarray(length, leftrange, rightrange);
+Console.WriteLine($"{string.Join(", ", massive)}");
+int max = massive[0];
+int min = massive[0];
+for (int i = 1; i < massive.Length; i++)
+{
+   if (massive[i] > max)
+  {
+   max = massive[i];
+
+  }
+  else
+  {
+    min = massive[i];
+  }
+}
+int diff = max - min;
+Console.WriteLine($" Максимальный элемент массива {max}, минимальный элемент массива {min}, их разница будет равна {diff}");
