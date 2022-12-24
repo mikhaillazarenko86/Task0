e21bf3c8d89e1c4783f6b8df4a23ef06fadec3c6
Михаил Лазаренко <mihaillazarenko@MacBook-Air-Mihail.local>
2022-12-24 08:11:52 +0300

    finished homework task 52

---
+++ HWTask52/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// ​
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+int ReadInt(string message)                                                 // Метод считывает целое число от пользователя.
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GetRandomMatrix(int rows, int columns, int leftRange = 0, int rightRange = 9)
+{
+    int[,] matr = new int[rows, columns];
+
+    var rand = new Random();
+    for(int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+
+    return matr;
+}
+
+void PrintMatrix(int[,] matr)
+{
+    for(int i = 0; i < matr.GetLength(0); i++)
+    {
+        Console.WriteLine();
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write(matr[i, j] + " ");
+        }
+    }
+}
+
+
+int rowsCount = ReadInt("Введите число строк");
+int columnsCount = ReadInt("Введите число столбцов");
+int[,] matrix = GetRandomMatrix(rowsCount, columnsCount);
+int sum = 0;
+PrintMatrix(matrix);
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+    
+    sum = sum + matrix[i, j];
+     
+    }
+    Console.WriteLine();
+    Console.WriteLine($"Сумма чисел в столбце равна {sum}");
+
+    double arithmeticmean = (double) sum / matrix.GetLength(0);
+    Console.WriteLine();
+    Console.WriteLine($" Среднеарифметическое в столбце с индексом {j} равно {arithmeticmean}");
+    sum = 0; 
+}
+
