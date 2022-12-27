9f4a597d7ea45fe6246587f993d9c9293d8a4645
Михаил Лазаренко <mihaillazarenko@192.168.0.9>
2022-12-27 09:33:25 +0300

    finished homework task 60

---
+++ HWTSak60/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел.
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+int[,,] myArray = new int[2, 2, 2];
+int b = 11;
+for (int i = 0; i < myArray.GetLength(0); i++)
+{
+    for (int j = 0; j < myArray.GetLength(1); j++)
+    {
+        for (int d = 0; d < myArray.GetLength(2); d++)
+        {
+            myArray[i,j,d] = b++;
+        }
+    }
+}
+for (int i = 0; i < myArray.GetLength(0); i++)
+{
+    Console.WriteLine($"Индекс уровня {i}");
+    for (int j = 0; j < myArray.GetLength(1); j++)
+    {
+        for (int d = 0; d < myArray.GetLength(2); d++)
+        {
+           Console.Write($"{myArray[i,j,d]} ({i}, {j}, {d})");
+        }
+       Console.WriteLine();
+    }
+    
+    
+}
