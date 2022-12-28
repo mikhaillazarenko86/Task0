09e5769305e5223f5e32de550f33815bf78b512a
Михаил Лазаренко <mihaillazarenko@192.168.0.9>
2022-12-28 07:42:51 +0300

    finished homework task 62

---
+++ HWTask62/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 62. Заполните спирально массив 4 на 4.
+
+// Например, на выходе получается вот такой массив:
+
+
+int[,] GetRandomMatrix(int rows, int columns, int leftRange = 0, int rightRange = 10)
+{
+    int[,] matr = new int[rows, columns];
+
+    var rand = new Random();
+    for(int j = 0; j < matr.GetLength(1); j++)
+    {
+        int i = 0;
+        matr[i, j] = rand.Next(leftRange, rightRange + 1);
+    }
+    for (int i = 1; i < matr.GetLength(0); i++)
+    {
+        int j =  matr.GetLength(1)-1;
+        matr[i, j] = rand.Next(leftRange, rightRange + 1);
+    }
+    for (int j = matr.GetLength(0)-2 ; j > 0; j--)
+    {
+        int i =  matr.GetLength(0)-1;
+        matr[i, j] = rand.Next(leftRange, rightRange + 1);
+    }
+    for (int j = 0 ; j < matr.GetLength(1)-1; j++)
+    {
+        int i =  matr.GetLength(0)-2;
+        matr[i, j] = rand.Next(leftRange, rightRange + 1);
+    }
+      for (int j = matr.GetLength(1)-1 ; j > 0; j--)
+    {
+        int i =  matr.GetLength(0)-3;
+        matr[i, j] = rand.Next(leftRange, rightRange + 1);
+    }
+    return matr;
+}
+
+void PrintMatrix(int[,] matr)
+{
+    for(int i = 0; i < matr.GetLength(0); i++)
+    {
+        Console.WriteLine();
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write(matr[i, j] + " ");
+        }
+    }
+}
+
+
+
+
+int[,] matrix = GetRandomMatrix(4, 4);
+PrintMatrix(matrix);
