27048cd01c726ead338c465755f4b5d5a4ff3e18
Михаил Лазаренко <mihaillazarenko@192.168.0.9>
2022-12-30 08:37:28 +0300

    finished homework task 66

---
+++ HWTask66/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+Console.WriteLine("Vvedite chislo 1");
+int a = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+Console.WriteLine("Vvedite chislo 2");
+int b = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+if (b < a) Console.WriteLine("Error");
+else
+{
+int Line(int a, int b)
+{
+if (a == b) return b; 
+int temp = Line(a+1,b);
+int sum = a + temp;
+return sum;
+}
+Console.Write(Line(a, b));
+}
